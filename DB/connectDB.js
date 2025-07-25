2d859314fb1d2293873893f979d3dfa61a919396
YashShrivastava14 <yshrivastava1415@gmail.com>
2025-07-25 23:24:43 +0530

    backendAPI

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-const url = "mongodb://0.0.0.0:27017/AdmissionPortalP"
+
 //mongo db cluster url
-const liveURL = 'mongodb+srv://yshrivastava1404:yash14@cluster0.0rn96sv.mongodb.net/?retryWrites=true&w=majority'
+
 const connectDB = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_LIVE_URL)
         console.log("Connected to MongoDB")
 
     }catch(err){
